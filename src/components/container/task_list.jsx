667cb06f984ddb8a4dc782b11537d7e896c54db8
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-10-05 00:38:20 -0300

    add loginForm and registerForm

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Task } from "../../models/task.class";
 import { LEVELS } from "../../models/levels.enum";
 import TaskComponent from "../pure/task";
@@ -10,7 +10,19 @@ const TaskList = () => {
     false,
     LEVELS.NORMAL
   );
-  const changeState = (id) => {
+
+  const [task, setTask] = useState([defaultTask]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("Task state has been modified");
+    setLoading(false);
+    return () => {
+      console.log("Task component is going to unmount...");
+    };
+  }, [task]);
+
+  const changeCompleted = (id) => {
     console.log(`ToDo: Cambiar estado de una tarea`);
   };
 
